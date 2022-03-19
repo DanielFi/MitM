4d2c480a845ddaba2c8410f3451e30504f32518b
AceWD <shalev.goo@gmail.com>
2022-03-19 02:20:30 +0200

    Handler Results Consts

--- mitm/handler_result.py
+++ mitm/handler_result.py
@@ -5,3 +5,7 @@ class HandlerResult:
     client_data: bytes
     server_data: bytes
     should_drop: bool
+
+
+HANDLER_RESULT_DROP = HandlerResult(b"", b"", True)
+HANDLER_RESULT_FORWARD = HandlerResult(b"", b"", False)
